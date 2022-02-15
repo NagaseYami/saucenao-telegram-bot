6fc21f83b571e8d8daee84e7378ff63eac8075e7
NagaseYami <nagaseyami@gmail.com>
2022-02-16 01:48:20 +0800

    fix ascii2d search

--- service/ascii2d.go
+++ service/ascii2d.go
@@ -2,9 +2,9 @@ package service
 
 import (
 	"path"
+	"strings"
 
 	"github.com/NagaseYami/telegram-bot/tool"
-	"github.com/go-rod/rod"
 	"github.com/go-rod/stealth"
 	"github.com/imroc/req/v3"
 	log "github.com/sirupsen/logrus"
@@ -24,7 +24,6 @@ type Ascii2dConfig struct {
 type Ascii2dService struct {
 	*Ascii2dConfig
 	reqClient *req.Client
-	page      *rod.Page
 }
 
 type Ascii2dResult struct {
@@ -36,7 +35,6 @@ type Ascii2dResult struct {
 
 func (service *Ascii2dService) Init() {
 	service.reqClient = req.C().SetOutputDirectory(service.TempDirectory)
-	service.page = stealth.MustPage(tool.Browser.RodBrowser)
 }
 
 func (service *Ascii2dService) Search(fileURL string) *Ascii2dResult {
@@ -47,24 +45,51 @@ func (service *Ascii2dService) Search(fileURL string) *Ascii2dResult {
 		return &Ascii2dResult{}
 	}
 
-	service.page.MustNavigate(ascii2dURL).MustWaitLoad()
-	service.page.MustElement("#file-form").MustSetFiles(path.Join(service.TempDirectory, fileName))
+	page := stealth.MustPage(tool.Browser.RodBrowser)
+	defer page.MustClose()
+	page = page.MustNavigate(ascii2dURL).MustWaitLoad()
 
-	service.page = service.page.MustElement("#file_upload > div > div.col-sm-1.col-xs-12 > button").MustClick().Page().MustWaitLoad()
-	colorThumb := ascii2dURL + *service.page.MustElement(firstResultThumbnailSelector).MustAttribute("src")
-	colorURL := *service.page.MustElement(firstResultURLSelector).MustAttribute("href")
+	if strings.Contains(page.MustInfo().Title, "Cloudflare") {
+		log.Warningln("检测到Cloudflare，撞墙了")
+	}
 
-	service.page = service.page.MustElement("body > div > div > div.col-xs-12.col-lg-8.col-xl-8 > div:nth-child(3) > div.detail-link.pull-xs-right.hidden-sm-down.gray-link > span:nth-child(2) > a").MustClick().Page()
+	page.MustElement("#file-form").MustSetFiles(path.Join(service.TempDirectory, fileName))
+	page = page.MustElement("#file_upload > div > div.col-sm-1.col-xs-12 > button").MustClick().Page().MustWaitIdle()
 
-	bovwThumbnail := ascii2dURL + *service.page.MustElement(firstResultThumbnailSelector).MustAttribute("src")
-	bovwURL := *service.page.MustElement(firstResultURLSelector).MustAttribute("href")
+	if strings.Contains(page.MustInfo().Title, "Cloudflare") {
+		log.Warningln("检测到Cloudflare，撞墙了")
+	}
 
-	log.Debugf("ascii2d搜索结果：\n色合：%s\n特征：%s\n", colorURL, bovwURL)
+	colorThumb := ascii2dURL + *page.MustElement(firstResultThumbnailSelector).MustAttribute("src")
+	var colorURL *string
+	if page.MustHas(firstResultURLSelector) {
+		colorURL = page.MustElement(firstResultURLSelector).MustAttribute("href")
+	} else {
+		colorURL = &page.MustInfo().URL
+		log.Infoln("色合搜索结果没有URL")
+	}
+
+	page = page.MustElement("body > div > div > div.col-xs-12.col-lg-8.col-xl-8 > div:nth-child(3) > div.detail-link.pull-xs-right.hidden-sm-down.gray-link > span:nth-child(2) > a").MustClick().Page().MustWaitIdle()
+
+	if strings.Contains(page.MustInfo().Title, "Cloudflare") {
+		log.Warningln("检测到Cloudflare，撞墙了")
+	}
+
+	bovwThumbnail := ascii2dURL + *page.MustElement(firstResultThumbnailSelector).MustAttribute("src")
+	var bovwURL *string
+	if page.MustHas(firstResultURLSelector) {
+		bovwURL = page.MustElement(firstResultURLSelector).MustAttribute("href")
+	} else {
+		bovwURL = &page.MustInfo().URL
+		log.Infoln("特征搜索结果没有URL")
+	}
+
+	log.Debugf("ascii2d搜索结果：\n色合：%s\n特征：%s\n", *colorURL, *bovwURL)
 
 	return &Ascii2dResult{
 		ColorThumbnail: colorThumb,
-		ColorURL:       colorURL,
+		ColorURL:       *colorURL,
 		BovwThumbnail:  bovwThumbnail,
-		BovwURL:        bovwURL,
+		BovwURL:        *bovwURL,
 	}
 }
